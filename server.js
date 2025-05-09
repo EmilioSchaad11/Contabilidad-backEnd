d65222db5d381ba4601f1229d20071ef46aabb79
jonyDLC8 <67179299+jonyDLC8@users.noreply.github.com>
2025-05-09 10:10:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const cuentadRoutes = require("./src/routes/cuentadRoutes");
-const cuentaiRoutes = require("./src/routes/cuentaiRoutes");
+mongoose.Promise = global.Promise;
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Conexión a MongoDB
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Conectado a MongoDB"))
-  .catch((err) => console.error("Error al conectar a MongoDB:", err));
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Rutas
-app.use("/cuentad", cuentadRoutes);
-app.use("/cuentai", cuentaiRoutes);
-
-// Servidor
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto ${PORT}`);
+mongoose.connect('mongodb://localhost:27017/proyecto_contabilidad').then(() => {
+  const PORT = 3500;
+  app.listen(PORT, () => console.log(`Escuchando http://localhost:${PORT}`));
 });
