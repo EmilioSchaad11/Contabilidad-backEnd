9d585d1fb5e2e41f86a9816d1a501b7e31df0434
jonyDLC8 <67179299+jonyDLC8@users.noreply.github.com>
2025-05-09 10:05:32 -0600

    Delete src directory para actualizar al nuevo

--- src/routes/balanceComprobacion.network.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/balance.controller");
-
-router.post("/", controller.agregar);
-router.get("/", controller.listar);
-router.delete("/:id", controller.eliminar);
-
-module.exports = router;
