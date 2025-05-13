4d246826f95173d6aa7b7332e21c0c8c064bbb9a
jonyDLC8 <67179299+jonyDLC8@users.noreply.github.com>
2025-05-13 08:33:38 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const routes = require("./index");  // Importamos las rutas desde index.js
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const partidaiRoutes = require("./src/routes/partidaiRoutes");
-const partidadRoutes = require("./src/routes/partidadRoutes");
 
-// Conexión a MongoDB
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Conectado a MongoDB"))
+  .then(() => {
+    console.log("Conectado a MongoDB");
+
+    
+    routes(app);  
+
+  
+    app.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
+  })
   .catch((err) => console.error("Error al conectar a MongoDB:", err));
-
-// Middlewares
-app.use(cors());
-app.use(express.json());
-app.use("/partidais", partidaiRoutes);
-app.use("/partidas", partidadRoutes);
-
-
-// Servidor
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en puerto${PORT}`);
-});
